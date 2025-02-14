2065e0b18418312567d8d0ce7402a9a274c0cf54
odutradev <joao.vitornl@gmail.com>
2025-02-14 10:47:47 -0300

    feat: Add space types

--- src/types/space.ts
+++ src/types/space.ts
@@ -1,31 +1,60 @@
-import { ObjectId } from "mongoose";
+export const SpaceRolePermissionsValues = [
+    "manage_categorys",
+    "manage_tickets",
+    "manage_modules",
+    "manage_classes",
+    "administrator", 
+    "manage_space",
+    "manage_roles",
+    "manage_users",
+    "manage_posts",
+    "manage_coins",
+    "manage_shop",
+    "manage_food",
+    "owner", 
+] as const;
 
-export type PostScopeType = "all" | "space" | "class" | "role" | "administrators";
-export type PostType = "default" | "report" | "warn";
+export type SpaceRolePermissions = typeof SpaceRolePermissionsValues[number];
 
-export interface PostModelType {
-    space: {
-        id: ObjectId;
-        name: string;
-    };
-    class: {
-        id: ObjectId;
-        name: string;
-    };
-    creator: {
-        id: ObjectId;
-        avatar: string;
-        name: string;
-    };
-    role: {
-        id: ObjectId;
-        name: string;
-    };
-    createAt: Date;
+export interface SpaceRoleType {
+    permissions: SpaceRolePermissions[];
+    system: boolean;
+    createAt?: Date;
+    name: string;
+};
+
+export interface SpaceModelType {
+    _id: string;
+    name: string;
+    status: "active" | "inactive";
+    createAt?: Date;
     lastUpdate?: Date;
-    content?: string;
-    title?: string;
-    attachments: string[];
-    type: PostType;
-    scope: PostScopeType;
+    description?: string;
+    images?: {
+        profile?: string;
+    };
+    owner: {
+        id: string;
+        name: string
+    };
+    badges?: any[];
+    metrics?: {
+        users: number;
+    };
+    roles: SpaceRoleType,
+    coins: number;
+    modules: {
+        economy: {
+            updateStatusAt?: Date;
+            lastUpdate?: Date;
+            moduleAlreadyUsed: boolean;
+            status: "active" | "inactive";
+            systemConfig: {
+                initialCoins: number;
+                coinPerAddeduser: number;
+                investmentsSlots: number;
+            };
+            config: {}
+        };
+    };
 };
